4cc6de74d70defe96d69393b2c4fbf95799eae2f
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-03-21 21:21:55 +0100

    Add Disable-UnusedFeatures.ps1

---
+++ System/NitroWin/System/Disable-UnusedFeatures.ps1
@@ -0,0 +1,2 @@
+Disable-WindowsOptionalFeature -FeatureName WorkFolders-Client -NoRestart -Online
+Disable-WindowsOptionalFeature -FeatureName Printing-PrintToPDFServices-Features -NoRestart -Online
