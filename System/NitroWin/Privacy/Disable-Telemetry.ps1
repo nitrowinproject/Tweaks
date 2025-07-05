56cd871965d11e33a8149f615e2ddc22674b9281
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-07-05 12:35:15 +0200

    Improve scripts

--- System/NitroWin/Privacy/Disable-Telemetry.ps1
+++ System/NitroWin/Privacy/Disable-Telemetry.ps1
@@ -1,2 +1,21 @@
-[System.Environment]::SetEnvironmentVariable("DOTNET_CLI_TELEMETRY_OPTOUT", "1", [System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable("POWERSHELL_TELEMETRY_OPTOUT", "1", [System.EnvironmentVariableTarget]::Machine)
+Write-Host "Disabling telemetry for various applications..."
+
+$variables = @(
+    "DOTNET_CLI_TELEMETRY_OPTOUT",
+    "POWERSHELL_TELEMETRY_OPTOUT"
+)
+
+foreach ($var in $variables) {
+    if ([System.Environment]::GetEnvironmentVariable($var, [System.EnvironmentVariableTarget]::Machine) -ne "1") {
+        try {
+            Write-Host "Setting $var to 1"
+            [System.Environment]::SetEnvironmentVariable($var, "1", [System.EnvironmentVariableTarget]::Machine)
+            Write-Host "$var has been set to 1" -ForegroundColor Green
+        }
+        catch {
+            Write-Host "Failed to set $var`: $_" -ForegroundColor Red
+        }
+    } else {
+        Write-Host "$var is already set to 1" -ForegroundColor Yellow
+    }
+}
