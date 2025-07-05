389575495addaca8a8c90f2a456a388c763b67cc
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-07-05 12:03:01 +0200

    Improve Disable-UnusedFeatures

--- System/NitroWin/System/Disable-UnusedFeatures.ps1
+++ System/NitroWin/System/Disable-UnusedFeatures.ps1
@@ -1,2 +1,17 @@
-Disable-WindowsOptionalFeature -FeatureName WorkFolders-Client -NoRestart -Online
-Disable-WindowsOptionalFeature -FeatureName Printing-PrintToPDFServices-Features -NoRestart -Online
+Write-Host "Disabling unused Windows features..."
+
+$features = @(
+    "WorkFolders-Client",
+    "Printing-PrintToPDFServices-Features"
+)
+
+foreach ($feature in $features) {
+    try {
+        Write-Host "Disabling feature: $feature..."
+        Disable-WindowsOptionalFeature -FeatureName $feature -NoRestart -Online
+        Write-Host "Feature '$feature' disabled successfully!" -ForegroundColor Green
+    }
+    catch {
+        Write-Host "Failed to disable feature '$feature': $_" -ForegroundColor Red
+    }
+}
