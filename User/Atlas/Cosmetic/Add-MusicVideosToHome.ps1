876716ceed7eb6f0332e561a1116b1a46e950b07
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-09-13 09:19:47 +0200

    Improve code formatting

--- User/Atlas/Cosmetic/Add-MusicVideosToHome.ps1
+++ User/Atlas/Cosmetic/Add-MusicVideosToHome.ps1
@@ -3,7 +3,7 @@ Write-Host "Adding Music and Videos to Home..." -ForegroundColor DarkGray
 $o = new-object -com shell.application
 $currentPins = $o.Namespace('shell:::{679f85cb-0220-4080-b29b-5540cc05aab6}').Items() | ForEach-Object { $_.Path }
 foreach ($path in @(
-        [Environment]::GetFolderPath('MyVideos'),
+        [Environment]::GetFolderPath('MyVideos')
         [Environment]::GetFolderPath('MyMusic')
     )) {
     if ($currentPins -notcontains $path) {
