6b1c039a70cd0da6e71fb49d466832a4388255b0
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-09-13 09:13:00 +0200

    Improve merge.ps1

--- merge.ps1
+++ merge.ps1
@@ -1,55 +1,51 @@
-$mergedRegistryUserFile = "NitroWin.Tweaks.User.reg"
-$mergedPowerShellUserFile = "NitroWin.Tweaks.User.ps1"
+class mergedFile {
+    [string]$name
+    [array]$contentPaths
 
-$mergedRegistrySystemFile = "NitroWin.Tweaks.System.reg"
-$mergedPowerShellSystemFile = "NitroWin.Tweaks.System.ps1"
-
-
-if (Test-Path $mergedRegistryUserFile) {
-    Remove-Item $mergedRegistryUserFile
+    mergedFile([hashtable]$Properties) { $this.Init($Properties) }
+    [void] Init([hashtable]$Properties) {
+        foreach ($Property in $Properties.Keys) {
+            $this.$Property = $Properties.$Property
+        }
+    }
 }
 
-if (Test-Path $mergedPowerShellUserFile) {
-    Remove-Item $mergedPowerShellUserFile
-}
+$mergedFiles = @(
+    [mergedFile]::new(@{
+        name = "NitroWin.Tweaks.User.reg"
+        contentPaths = (Get-ChildItem -Path "User" -Filter "*.reg" -Recurse)
+    })
+    [mergedFile]::new(@{
+        name = "NitroWin.Tweaks.User.ps1"
+        contentPaths = (Get-ChildItem -Path "User" -Filter "*.ps1" -Recurse)
+    })
+    [mergedFile]::new(@{
+        name = "NitroWin.Tweaks.System.reg"
+        contentPaths = (Get-ChildItem -Path "System" -Filter "*.reg" -Recurse)
+    })
+    [mergedFile]::new(@{
+        name = "NitroWin.Tweaks.System.ps1"
+        contentPaths = (Get-ChildItem -Path "System" -Filter "*.ps1" -Recurse)
+    })
+)
 
+foreach ($file in $mergedFiles) {
+    if (Test-Path $file.name) {
+        Remove-Item $file.name
+    }
 
-if (Test-Path $mergedRegistrySystemFile) {
-    Remove-Item $mergedRegistrySystemFile
-}
+    if ($file.name.EndsWith(".reg")) {
+        Add-Content -Path $file.name -Value "Windows Registry Editor Version 5.00"
+        foreach ($contentPath in $file.contentPaths) {
+            $content = Get-Content $contentPath.FullName
+            Add-Content -Path $file.name -Value ($content | Select-Object -Skip 1)
+        }
+    }
 
-if (Test-Path $mergedPowerShellSystemFile) {
-    Remove-Item $mergedPowerShellSystemFile
-}
-
-
-$registryTweakUserFiles = Get-ChildItem -Path "User" -Filter "*.reg" -Recurse
-$powershellTweakUserFiles = Get-ChildItem -Path "User" -Filter "*.ps1" -Recurse
-
-$registryTweakSystemFiles = Get-ChildItem -Path "System" -Filter "*.reg" -Recurse
-$powershellTweakSystemFiles = Get-ChildItem -Path "System" -Filter "*.ps1" -Recurse
-
-
-Add-Content -Path $mergedRegistryUserFile -Value "Windows Registry Editor Version 5.00"
-
-foreach ($file in $registryTweakUserFiles) {
-    $content = Get-Content $file.FullName
-    Add-Content -Path $mergedRegistryUserFile -Value ($content | Select-Object -Skip 1)
-}
-
-foreach ($file in $powershellTweakUserFiles) {
-    $content = Get-Content $file.FullName
-    Add-Content -Path $mergedPowerShellUserFile -Value $content
-}
-
-Add-Content -Path $mergedRegistrySystemFile -Value "Windows Registry Editor Version 5.00"
-
-foreach ($file in $registryTweakSystemFiles) {
-    $content = Get-Content $file.FullName
-    Add-Content -Path $mergedRegistrySystemFile -Value ($content | Select-Object -Skip 1)
-}
-
-foreach ($file in $powershellTweakSystemFiles) {
-    $content = Get-Content $file.FullName
-    Add-Content -Path $mergedPowerShellSystemFile -Value $content
+    else {
+        foreach ($contentPath in $file.contentPaths) {
+            $content = Get-Content $contentPath.FullName
+            Add-Content -Path $file.name -Value $content
+        }
+    }
 }
