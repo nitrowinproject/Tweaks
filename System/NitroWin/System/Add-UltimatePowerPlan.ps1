7ba393f5ff826737a8549d0eb26ae970d8bed35b
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2025-09-13 09:12:13 +0200

    Improve Ultimate Power Plan script

--- System/NitroWin/System/Add-UltimatePowerPlan.ps1
+++ System/NitroWin/System/Add-UltimatePowerPlan.ps1
@@ -1,7 +1,12 @@
+Write-Host "Adding Ultimate Power Plan..." -ForegroundColor DarkGray
+
 $keywords = @("Enterprise", "Workstation")
 $regex = [string]::Join("|", $keywords)
 
 if (-Not (Get-WindowsEdition -Online).Edition -match $regex) {
-    Write-Host "Adding Ultimate Power Plan..." -ForegroundColor DarkGray
     Start-Process -Wait -NoNewWindow -FilePath "powercfg.exe" -ArgumentList "/duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61"
+    Write-Host "Added Ultimate Power Plan!" -ForegroundColor Green
+}
+else {
+    Write-Host "Not adding Ultimate Power Plan because it's already installed..."
 }
