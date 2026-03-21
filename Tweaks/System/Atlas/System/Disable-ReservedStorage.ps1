f77ffacda50f8ac05303f7a197542cba3cc04900
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2026-03-21 18:27:29 +0100

    Rewrite and add improve tweaks

--- Tweaks/System/Atlas/System/Disable-ReservedStorage.ps1
+++
@@ -1,3 +0,0 @@
-Write-Host "Disabling Reserved Storage..." -ForegroundColor DarkGray
-
-Start-Process -Wait -NoNewWindow -FilePath "dism.exe" -ArgumentList "/Online /Set-ReservedStorageState /State:Disabled"
